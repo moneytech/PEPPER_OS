3ae43623ccab52e268ab23aba0410db175c671fb
root <root@localhost.localdomain>
2019-12-24 01:03:13 +0100

    first commit

--- arch_ia32/kernel/initialisation/exceptions/exception_asm.asm
+++ arch_ia32/kernel/initialisation/exceptions/exception_asm.asm
@@ -4,21 +4,12 @@ extern exception
 
 section .text
 
-<<<<<<< HEAD:arch_ia32/kernel/initialisation/exceptions/exception_asm.asm
-;eflags|cs:eip|error code
-protection_fault_handler :
-    pop eax
-    mov dword [_error_code_] , eax
-    call protection_fault
-=======
-
 ;eflags|cs:eip|error code
 exception_handler :
     pop eax ;take Error code and analyse it
     push eax
     call exception
     pop eax
->>>>>>> ecab81465069d2e21f3c6f2af2ed85c8cf8ca835:arch_ia32/kernel/initialisation/exceptions/exception_err_code_asm.asm
     iret
     
 section .data 
